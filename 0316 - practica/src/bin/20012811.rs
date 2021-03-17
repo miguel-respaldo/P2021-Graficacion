33bbb80eeab55b0d63e4aeb57f42dad4fde93add
EndarkG <gabriel_endark@hotmail.com>
2021-03-16 18:32:33 -0600

    enter

---
+++ 0316 - practica/src/bin/20012811.rs
@@ -0,0 +1,6 @@
+fn main() {
+    println!("Hi!");
+    println!("Alumno: Arce Higuera Jesús Gabriel.");
+    println!("Matrícula: 20012811.");
+    println!("Correo Institucional: za20012811@zapopan.tecmm.edu.mx");
+}
