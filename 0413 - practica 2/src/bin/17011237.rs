a7bdbd20072820c8bf6ab31cf33b6e56e6dba60a
Oscar Mauricio Trujillo Enriquez <za17011237@zapopan.tecmm.edu.mx>
2021-04-13 18:16:16 -0500

    Oscar

--- 0413 - practica 2/src/bin/17011237.rs
+++ 0413 - practica 2/src/bin/17011237.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hi!");
+//Oscar Mauricio Trujillo Enriquez
+
+fn main(){
+    let x = 10;
+    println!("El numero de fibonacci de {}! = {}", x,fibonacci(x));
+}
+
+fn fibonacci(n:u32) -> u32{
+    if n == 1 || n == 2 {
+        1
+    } else {
+        fibonacci(n-1) + fibonacci(n-2)
+    }
 }
