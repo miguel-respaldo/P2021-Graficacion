b7a3aaac78a880c29d05cfe1605b044e2ac27bf4
Splytek <splytek@gmail.com>
2021-04-13 18:25:42 -0500

    Hugo Emmanuel Gonzalez Gomez

--- 0413 - practica 2/src/bin/17011650.rs
+++ 0413 - practica 2/src/bin/17011650.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hi!");
+
+  //Ejercicio Fibonacci
+  
+    let x:i32 = 0, y:i32 = 0, z:i32 = 0, w:i32 = 1;
+
+    (int i=3;i<=x;i++)
+
+    for x in 3 0..10 {
+    
+      y=z+w;
+      z=w;
+      w=y;
+      
+      println!("{}",y); 
+      
+    }
+
 }
+
+
