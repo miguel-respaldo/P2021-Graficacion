af35305405c8c4094b98efb4376a265b80e75ad8
Abraham Lomeli <abram.atzin@gmail.com>
2021-04-13 18:22:42 -0500

    fibonnacci

--- 0413 - practica 2/src/bin/17012453.rs
+++ 0413 - practica 2/src/bin/17012453.rs
@@ -1,3 +1,14 @@
+//Abraham Lomeli 17012453
+
 fn main() {
-    println!("Hi!");
+    let mut x1 = 0;
+    let mut x2 = 1;
+    let mut z = 0;
+//    let mut i = 0;
+    for i in 0..5{
+        z = x1+x2;
+    println!("Valor x: {}", z);
+    x1=x2;
+    x2=z;
+  }
 }
