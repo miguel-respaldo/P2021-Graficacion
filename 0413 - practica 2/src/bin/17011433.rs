3ac16d643b36e536ed405e20e61dc61cc93db42f
Moisés Alejandro Viayra Mercado <moisescoyote3@gmail.com>
2021-04-13 18:13:03 -0500

    Subi el fibonnaci

--- 0413 - practica 2/src/bin/17011433.rs
+++ 0413 - practica 2/src/bin/17011433.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hi!");
+    for num:u32 in 0..15{
+println!("Fib({})={}", num, fib(n:num));
+    }
 }
+fn fib (n:u32) -> u32 (
+    if n <=0 {
+        return 0;
+
+    }
+    else if n== 1{
+        return 1;
+    }
+    fib(n:n-1) + fib(n:n-2)
+)
