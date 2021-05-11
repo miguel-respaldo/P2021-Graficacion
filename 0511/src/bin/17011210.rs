e3c1c5c63ff5cee80fa43317821669ae7c326ddc
Héctor Uriel Aguilera Peña <jimowww@hotmail.com>
2021-05-11 18:14:05 -0500

    Factorial Héctor Uriel Aguilera Peña

--- 0511/src/bin/17011210.rs
+++ 0511/src/bin/17011210.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hi!");
+    for i in 1..9 {
+        println!("{}", fact(i));
+    }
+}
+
+fn fact(num: i32) -> i32 {
+    if num == 0 {
+        1
+    }else{
+        num * fact(num - 1)
+    }
 }
