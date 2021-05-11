415d615f39602088e715b16bb45458f9a9f7af0f
Christopher Esparza <christopher52@live.com.mx>
2021-05-11 18:19:43 -0500

    factorial

--- 0511/src/bin/17011701.rs
+++ 0511/src/bin/17011701.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hi!");
+fn factorial(x: u32) -> u32 {
+    if x < 2 {
+        1
+    } else {
+        x * factorial(x - 1)
+    }
 }
+
+fn main() {
+    println!("{}",factorial(5));
+  }
