fd5462810cf17756f1cd22f286a7f7c32506b166
Edgar Adrian Luna Lopez <za17011427@zapopan.tecmm.edu.mx>
2021-05-11 18:21:31 -0500

    Mi programa

--- 0511/src/bin/17011427.rs
+++ 0511/src/bin/17011427.rs
@@ -1,3 +1,18 @@
+use std;
+import std::io;
+
+fn factorial(x: int) -> uint {
+  if (x <= 1) {
+    ret 1u;
+  } else {
+    ret (x as uint) * factorial(x - 1);
+  }
+}
+
 fn main() {
-    println!("Hi!");
+  let i = 0;
+  while i <= 16 {
+    io::println(#fmt("%d! = %u", i, factorial(i)));
+    i = i + 1;
+  }
 }
