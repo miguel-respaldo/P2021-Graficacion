1ad003e414842e752e1ed4f6beae313e56b0b71f
Hugo Emmanuel Gonzalez Gomez <splytek@gmail.com>
2021-05-11 18:16:37 -0500

    Factorial (v1) - Hugo Emmanuel Gonzalez Gomez

--- 0511/src/bin/17011650.rs
+++ 0511/src/bin/17011650.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hi!");
-}
+//FACTORIAL
+/* Alumno: Hugo Emmanuel Gonzalez Gomez
+   Materia: Graficacion 
+   Semestre: 8vo*/
+
+   use std::io;
+
+   fn main(){
+       let _i:i32 = 1;
+   
+       let mut numero = String::new();
+       println!("Escribe un numero: ");
+       io::stdin().read_line(&mut numero);
+       let numero = numero.trim();
+       let numero:i32 = numero.parse().unwrap();
+   
+       for i in 1..numero{
+         let numero = numero * i;
+         println!("El factorial es: {}", numero);
+       }
+   
+   }
