4aa7958f5912c4797fec8dca4a8530c172db12f2
Oscar Mauricio Trujillo Enriquez <za17011237@zapopan.tecmm.edu.mx>
2021-05-11 18:52:49 -0500

    Oscar Trujillo

--- 0511/src/bin/17011237.rs
+++ 0511/src/bin/17011237.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hi!");
+//Oscar Mauricio Trujillo Enriquez
+
+use std::io;
+use std::str::FromStr;
+
+fn main(){
+    println!("Ingresa un numero: ");
+    
+    let mut x = String::new();
+    io::stdin().read_line(&mut x);
+    
+    let x = x.trim(); 
+    let x: u64 = x.parse().unwrap();
+
+    println!("FACTORIAL DE {}! = {}",x,factorial(x));
+}
+
+fn factorial(y : u64) -> u64{
+    if y == 0 {
+        1
+    } else {
+        y * factorial(y - 1)
+    }
 }
