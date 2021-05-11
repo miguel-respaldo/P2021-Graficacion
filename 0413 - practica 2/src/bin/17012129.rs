7873585188cc7bbc3a6f609730ce600dc5a17244
José Ricardo Siordia Alatorre <richysiordia@gmail.com>
2021-05-11 17:11:51 -0500

    Soy Ricardo

--- 0413 - practica 2/src/bin/17012129.rs
+++ 0413 - practica 2/src/bin/17012129.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hi!");
+    println!("José Ricardo Siordia Alatorre");
+    println!("Result: {}", fibo(3));
+}    
+    
+fn fibo(num: u32) -> u32 {
+    if num <= 1{
+        return num;
+    }
+    return fibo(num-1) + fibo(num-2);
 }
