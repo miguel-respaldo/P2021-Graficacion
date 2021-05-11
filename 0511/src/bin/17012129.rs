57dd6658f37e6c09e4d4ffe723ee8d51c857ead2
José Ricardo Siordia Alatorre <richysiordia@gmail.com>
2021-05-11 18:16:29 -0500

    Factorial Ricardo Siordia Alatorre

--- 0511/src/bin/17012129.rs
+++ 0511/src/bin/17012129.rs
@@ -1,3 +1,13 @@
+fn factorial(num: u128) -> u128 {
+    match num {
+        0 => 1,
+        1 => 1,
+        _ => factorial(num - 1) * num,
+    }
+}
+
 fn main() {
-    println!("Hi!");
+    println!("Soy José Ricardo Siordia Alatorre");
+    let x = factorial(21);
+    println!("El valor del factorial de 21 es:{} ", x);
 }
