21b0bf59396868b5c934ddd2649b6c1ab5d44efa
Miguel Bernal Marin <miguel.bernal@zapopan.tecmm.edu.mx>
2021-05-11 18:48:51 -0500

    mi factorial

--- 0511/src/bin/290.rs
+++ 0511/src/bin/290.rs
@@ -1,3 +1,34 @@
+use std::io;
+
+fn factorial(n:u128) -> u128 {
+    match n {
+        0 | 1 => 1,
+        _     => factorial(n-1) * n
+    }
+}
+
 fn main() {
-    println!("Hi!");
+
+    println!("Escribe el un número para calcular su factorial: ");
+
+    let mut numero = String::new();
+
+    io::stdin()
+        .read_line(&mut numero)
+        .expect("Error al leer el número");
+
+    let numero = numero
+        .trim()           // Quita espacios (el Enter)
+        .to_string()      // Lo hace String
+        .parse()          // Lo convierte a Número
+        .unwrap_or(0);   // Si hay un error, devuelve -1
+
+    if numero == 0 {
+        println!("No se ingreso un número correctamente");
+    } else {
+
+        let resultado = factorial(numero);
+
+        println!("El resultado es {}", resultado);
+    }
 }
