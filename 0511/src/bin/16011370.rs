beb9fdb7c8125779769fd0258cf91a2990ca0f5f
Luis Fernando Rubio Zambrano <za16011370@zapopan.tecmm.edu.mx>
2021-05-11 20:39:52 -0500

    factorial Luis Fernando Rubio Zambrano

--- 0511/src/bin/16011370.rs
+++ 0511/src/bin/16011370.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hi!");
+fn factor(num: u64) -> u64 {
+    match num {
+        0 => 1,
+        1 => 1,
+        _ => factor(num - 1) * num,
+    }
 }
+
+fn main() {
+    let x = factor(8);
+    println!("Tu factorial es: { } ", x);
+} 
