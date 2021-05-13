669e665213e3b3ff9ed5c348638cae4e041faba0
Miguel Angel Lopez Alcantar <00mala279@gmail.com>
2021-05-13 12:23:55 -0500

    Miguel Angel Lopez Alcantar

--- 0413 - practica 2/src/bin/17012084.rs
+++ 0413 - practica 2/src/bin/17012084.rs
@@ -1,3 +1,22 @@
+// Miguel Angel Lopez Alcantar
+
+fn fib(n: u32) -> u32 {
+    match n {
+        0 => 1,
+        1 => 1,
+        _ => fib(n-1) + fib(n -2),
+    }
+}
+
+
+fn print_fb(n: u32){
+	println!("Fibonacci de {} es {}", n, fib(n));
+}
+
+
 fn main() {
-    println!("Hi!");
+	print_fb(10);
+	print_fb(4);    
+	print_fb(20);
+	print_fb(5);
 }
--- 0511/src/bin/17012084.rs
+++ 0511/src/bin/17012084.rs
@@ -1,3 +1,24 @@
+// Miguel Angel Lopez Alcantar
+
+
+fn factorial(x: u32) -> u32 {
+    match x {
+        0 => 1,
+        1 => 1,
+        _ => factorial(x - 1) * x,
+    }
+}
+
+
+fn print_fact(x: u32){
+	println!("El factorial de {} es: {} ", x, factorial(x));
+}
+
+
 fn main() {
-    println!("Hi!");
+	print_fact(4);
+	print_fact(7);
+	print_fact(6);
+	print_fact(9);
+	print_fact(2);
 }
