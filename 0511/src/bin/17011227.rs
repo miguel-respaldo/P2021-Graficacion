5c537f5be49daa01ceda79b860f565957e574c3f
Luis Carlos Moron <za17011227@zapopan.tecmm.edu.mx>
2021-05-14 09:46:56 -0500

    Factorial luis carlos moron

--- 0511/src/bin/17011227.rs
+++ 0511/src/bin/17011227.rs
@@ -1,3 +1,28 @@
+use std::io;
+
 fn main() {
-    println!("Hi!");
+    println!("===Factorial===");
+    loop{
+        let mut n = String::new();
+        println!("Ingresa un numero:");
+
+        io::stdin().read_line(&mut n).expect("Fallo al leer el numero");
+        println!("Resultado: ");
+        
+        let n: u32 = match n.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("{}", factorial(n));
+        println!();
+    }
+}
+fn factorial (n: u32) -> u32 {
+    if n <= 1 {
+        1
+    }else{
+        println!("x  {} =",n);
+        
+        n*factorial(n - 1)
+    }
 }
