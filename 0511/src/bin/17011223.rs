137bd7607bcbe146e3f38c37af5f16d35235506a
Oscar Daniel Ibañez <za17011223@zapopan.tecmm.edu.mx>
2021-05-14 10:23:00 -0500

    OSCAR DANIEL IBAÑEZ FACTORIAL

--- 0511/src/bin/17011223.rs
+++ 0511/src/bin/17011223.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hi!");
+use std::io,
+
+
+
+fn main (){
+
+    let _i:i32=1;
+    
+    let mut num = String::new();
+    println!("Calculadora de factoriales"\n 
+    "ingresa el número a calcular");
+    io::stdin().read_line(&mut num);
+
+    let num=num.trim();
+    let num:i32=num.parse().unwrap();
+
+     //   for elem in iter {
+            
+       // }
+    for i in 1..num{
+        let num=num*i;
+        println! ("RESULTADO:", num);
+
+    }
+
 }
